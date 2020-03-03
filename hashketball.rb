40cbf9c4eadb18f4b926e55170e1a66bf1cd45bc
Dakota Willingham <kota@vaga.tk>
2020-03-03 23:41:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -323,9 +323,9 @@ def player_with_longest_name
     team.each do |(attribute, data)|
       if attribute == :players
         data.each do |player|
-          if player[:points] > most_points
-            most_points = player[:points]
-            highest_scorer = player[:player_name]
+          if player[:player_name].length > longest_length
+            longest_length = player[:player_name].length
+            longest_name = player[:player_name]
             # binding.pry
           end
         end
