3394fa43ac594573c0d45328f4574663782ac99e
Dakota Willingham <kota@vaga.tk>
2020-03-03 22:42:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,12 +198,9 @@ def player_numbers(team_name)
     if team[:team_name] == team_name
       team.each do |(attribute, data)|
         if attribute == :players
-          attribute.each do |(key, value)|
-            binding.pry
-            if key == :number
-              number_list << value
-              binding.pry
-            end
+          player_counter = 0
+          while player_counter < data.length
+
           end
         end
       end
