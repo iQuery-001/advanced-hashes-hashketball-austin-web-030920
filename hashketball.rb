b0cdfb0c7bbda55452dc8a383db0355f76fe97ea
Dakota Willingham <kota@vaga.tk>
2020-03-03 22:52:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -218,7 +218,7 @@ def player_stats(given_player)
       if attribute == :players
         data.each do |player|
           # player.each do |(stat, value)|
-            # binding.pry
+            binding.pry
           end
         end
       end
